a036f94f7d917542cccad2478773be72b5e67cea
sunjae-kim <kimsj9484@gmail.com>
2019-02-09 13:27:25 +0900

    Modify model
     - Change model names to singular
     - Modify cafe model schema

--- src/model/cafe.js
+++ src/model/cafe.js
@@ -2,20 +2,33 @@ const { Schema, model } = require('mongoose');
 const Joi = require('joi');
 
 const cafeSchema = new Schema({
-  property: { type: Number, required: true },
-  location: { type: String, require: true },
+  property: { type: Number, default: 0 },
+  title: { type: String, required: true },
+  images: [String],
+  contact: String,
+  addresses: { type: [String], required: true },
+  location: Object,
+  openingHours: [Object],
+  menus: [Object],
+  homepage: String,
+  convenience: String,
+  description: String,
   tagId: { type: Schema.Types.ObjectId, ref: 'Tags' },
-});
-
-cafeSchema.pre('save', (next) => {
-  this.location = this.location.toUpperCase();
-  next();
 });
 
 const validateCafe = (cafe) => {
   const schema = {
-    property: Joi.Number().required(),
-    location: Joi.string().required(),
+    property: Joi.number(),
+    title: Joi.string().required(),
+    images: Joi.array(),
+    constact: Joi.string(),
+    addresses: Joi.array(),
+    location: Joi.object(),
+    openingHours: Joi.array(),
+    menus: Joi.array(),
+    homepage: Joi.string(),
+    convenience: Joi.string(),
+    description: Joi.string(),
     tagid: Joi.string(),
   };
   return Joi.validate(cafe, schema);
